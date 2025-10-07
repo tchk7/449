673a2cacaba3032622714f80c51fc496a7a1155a
Travon <tchk7@umkc.edu>
2025-10-06 21:44:47 -0500

    First commit for sprint 2. Initial Board Class

---
+++ sprint_2/board.py
@@ -0,0 +1,29 @@
+
+
+class Board:
+    def __init__(self, size = 3):
+        self.size = size
+        self._create_grid()
+
+    def _create_grid(self):
+        """Create an empty grid"""
+        self.grid = []
+
+        for rowIndex in range(self.size):
+            row = []
+            for colIndex in range(self.size):
+                row.append("")
+            self.grid.append(row)
+
+    def is_empty(self, row, col):
+        """Check if cell is empty"""
+        return self.grid[row][col] == ""
+
+    def is_full(self):
+        """Check if board is full"""
+        for rowIndex in range(self.size):
+            for colIndex in range(self.size):
+                if self.is_empty(rowIndex, colIndex):
+                    return False
+        return True
+
