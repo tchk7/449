6503749b7bce328ded2b74d6c2ad1ca74fa3eb6f
Travon <tchk7@umkc.edu>
2025-10-14 10:57:10 -0500

    Player class added.

---
+++ sprint_2/player.py
@@ -0,0 +1,19 @@
+
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.letter = ""
+
+    def letter(self, letter_choice):
+        if letter_choice.upper() in ("S", "O"):
+            self.letter = letter_choice
+        else:
+            raise ValueError("Letter must be an 'S' or 'O'")
+
+
+    def make_move(self, board, row, col):
+
+        board.put_letter(row, col, self.letter)
+
