7f56f277b7ed6f891208049a67ddb167b00b108c
Travon <tchk7@umkc.edu>
2025-10-29 20:21:14 -0500

    Simple and General games.

--- sprint_3/models/simple_game.py
+++ sprint_3/models/simple_game.py
@@ -4,3 +4,18 @@ from sprint_3.models.base_game import BaseGame
 class SimpleGame(BaseGame):
     def __init__(self, board, players):
         super().__init__(board, players)
+
+    def handle_move(self, row, col, player):
+
+        letter = self.board.get_cell(row, col)
+
+        if self.check_sos(row, col, letter) > 0:
+            self.game_over = True
+            self.players[player].score = 1
+            return "WIN"
+
+        if self.board.is_full():
+            self.game_over = True
+            return "DRAW"
+
+        return "CONTINUE"
