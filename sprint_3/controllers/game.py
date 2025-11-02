c5087b414dd4cedf3bf8a2d5c9b66457fb402354
Travon <tchk7@umkc.edu>
2025-11-02 17:13:18 -0600

    game added.

---
+++ sprint_3/controllers/game.py
@@ -0,0 +1,117 @@
+from functools import partial
+
+from sprint_2.model.board import Board
+from sprint_3.models.general_game import GeneralGame
+from sprint_3.models.simple_game import SimpleGame
+
+
+class Game():
+    def __init__(self, game_ui):
+        self.game_ui = game_ui
+        self.board_ui = self.game_ui.get_board_ui()
+        self.player_uis = self.game_ui.get_player_uis()
+
+        self.players = [self.player_uis[0].get_player(), self.player_uis[1].get_player()]
+
+        self.current_player = 0
+
+        self.buttons = []
+        self.connect_buttons()
+
+        self.game_mode = "Simple"
+        self.game_type = SimpleGame(self.board_ui.get_board(), self.players)
+
+        self.game_ui.simple_radio.toggled.connect(self.update_game_mode)
+        self.game_ui.general_radio.toggled.connect(self.update_game_mode)
+
+        self.game_ui.new_game.clicked.connect(self.start_new_game)
+
+    def connect_buttons(self):
+        self.buttons = self.board_ui.get_buttons()
+        for row_index, row in enumerate(self.buttons):
+            for col_index, btn in enumerate(row):
+                btn.clicked.connect(partial(self.handle_click, row_index, col_index))
+
+    def handle_click(self, row, col):
+        board = self.board_ui.get_board()
+
+        if not board.is_empty(row, col) or self.game_type.game_over:
+            return
+
+        player_ui = self.player_uis[self.current_player]
+
+        letter = player_ui.get_selected_letter()
+
+        board.put_letter(row, col, letter)
+        self.buttons[row][col].setText(letter)
+
+        status = self.game_type.handle_move(row, col, self.current_player)
+
+        switch_player = False
+
+        if status == "WIN":
+            winner = self.players[self.current_player].name
+            self.game_ui.show_winner_message(f"{winner} wins.")
+
+        elif status == "DRAW":
+            self.game_ui.draw_message()
+
+        elif status == "Score":
+            switch_player = False
+            self.game_ui.player_turn_label.setText(f"{self.players[self.current_player].name} scores. Go again.")
+
+        elif status == "Continue":
+            switch_player = True
+
+        elif status == "We have a winner.":
+            player_1_score = self.players[0].score
+            player_2_score = self.players[1].score
+
+            if player_1_score > player_2_score:
+                winner = self.players[0].name
+            else:
+                winner = self.players[1].name
+
+            self.game_ui.show_winner_message(f"{winner} wins with {max(player_1_score, player_2_score)} points.")
+
+        elif status == "We have a draw.":
+            self.game_ui.draw_message()
+
+        if switch_player and not self.game_type.game_over:
+            self.current_player = 1 - self.current_player
+
+        if not self.game_type.game_over:
+            next_player = self.players[self.current_player]
+            self.game_ui.player_turn_label.setText(f"{next_player.get_player().name}'s Turn")
+
+
+    def start_new_game(self):
+
+        text = self.game_ui.board_size_text_box.text()
+        if text.isnumeric():
+            size = int(text)
+        else:
+            size = 3
+
+        board = Board(size)
+
+        self.board_ui = self.game_ui.build_board_ui(board)
+        self.connect_buttons()
+
+        for player in self.players:
+            player.score = 0
+
+        self.current_player = 0
+
+        if self.game_mode == "Simple":
+            self.game_type = SimpleGame(board, self.players)
+        else:
+            self.game_type = GeneralGame(board, self.players)
+
+        self.game_ui.player_turn_label.setText(f"{self.players[0].get_player().name}'s Turn")
+
+    def update_game_mode(self):
+        if self.game_ui.simple_radio.isChecked():
+            self.game_mode = "Simple"
+        elif self.game_ui.general_radio.isChecked():
+            self.game_mode = "General"
---
+++ sprint_3/views/game_ui.py
@@ -0,0 +1,103 @@
+import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QIntValidator
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QCheckBox, QLabel, QLineEdit, QGridLayout, QApplication, \
+    QRadioButton, QPushButton, QMessageBox
+
+from sprint_2.controller.game import Game
+from sprint_2.model.board import Board
+from sprint_2.model.player import Player
+from sprint_2.view.board_ui import BoardUI
+from sprint_2.view.player_ui import PlayerUI
+
+
+class GameUI(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("SOS Game")
+
+        self.game_board = Board()
+        self.board_ui = BoardUI(self.game_board)
+        self.blue_player = Player("Blue")
+        self.blue_player_ui = PlayerUI(self.blue_player)
+        self.red_player = Player("Red")
+        self.red_player_ui = PlayerUI(self.red_player)
+
+        self.new_game = QPushButton("New Game")
+
+        self.game_type_label = QLabel("Game Mode:")
+        self.simple_radio = QRadioButton("Simple Game")
+        self.simple_radio.setChecked(True)
+        self.general_radio = QRadioButton("General Game")
+
+        radio_layout = QHBoxLayout()
+        radio_layout.addWidget(self.game_type_label)
+        radio_layout.addWidget(self.simple_radio)
+        radio_layout.addWidget(self.general_radio)
+
+        board_size_label = QLabel("Board Size")
+        self.board_size_text_box = QLineEdit()
+        self.board_size_text_box.setValidator(QIntValidator(3, 12))
+        self.board_size_text_box.setPlaceholderText("Choose 3 to 12")
+
+        board_size_layout = QHBoxLayout()
+        board_size_layout.addWidget(board_size_label)
+        board_size_layout.addWidget(self.board_size_text_box)
+
+
+        self.player_turn_label = QLabel(f"{self.blue_player.name}'s Turn", alignment=Qt.AlignCenter)
+
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
+
+        self.grid.addLayout(radio_layout, 0, 0, 1, 2)
+        self.grid.addLayout(board_size_layout, 0, 3, 1, 1)
+        self.grid.addWidget(QWidget(), 0, 2, 1, 1)
+        self.grid.addWidget(self.blue_player_ui, 2, 0, 1, 1)
+        self.grid.addWidget(self.board_ui, 1, 1, 3, 3)
+        self.grid.addWidget(self.red_player_ui, 2, 4, 1, 1)
+        self.grid.addWidget(self.player_turn_label, 4, 0, 1, 5)
+        self.grid.addWidget(self.new_game, 0, 4, 1, 1)
+
+        self.resize(500, 500)
+
+        self.controller = Game(self)
+
+    def get_player_uis(self):
+        return [self.blue_player_ui, self.red_player_ui]
+
+    def get_board_ui(self):
+        return self.board_ui
+
+    def build_board_ui(self, board):
+
+        self.grid.removeWidget(self.board_ui)
+
+        self.board_ui.deleteLater()
+
+        self.game_board = board
+        self.board_ui = BoardUI(self.game_board)
+        self.grid.addWidget(self.board_ui, 1, 1, 3, 3)
+
+        return self.board_ui
+
+    def show_winner_message(self, message):
+        QMessageBox.information(self, "Game Over", message)
+
+    def winner_message(self):
+        QMessageBox.information(self, "Game Over", "We have a winner.")
+
+    def draw_message(self):
+        QMessageBox.information(self, "Game Over", "We have a draw.")
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = GameUI()
+    window.show()
+    sys.exit(app.exec())
+
+
